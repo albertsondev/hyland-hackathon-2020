79b8e843226766141c66d1393cf2a40807fa8138
Tom Lombardo <lombowatch354@gmail.com>
2020-01-05 01:25:29 -0500

    Backend Search Function

--- backend/toolpool/tool_pool/views.py
+++ backend/toolpool/tool_pool/views.py
@@ -2,6 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Listing
 
 def index(request):
-    return render(request, 'tool_pool/skeleton_page.html')
+    context = {'authenticated':False}
+    return render(request, 'tool_pool/page-skeleton.html', context)
+
+def search(request):
+    query = request.GET['listing-search']
+    context = {'query': query}
+    raw = "SELECT * FROM tool_pool_listing WHERE title LIKE '" + query + "%' ;"
+    results = Listing.objects.raw(raw)
+    if list(results):
+        context['found'] = True
+        context['result_data'] = [result.__dict__ for result in results]
+    else:
+        context['found'] = False
+    return render(request, 'tool_pool/page-listing-search.html', context)
